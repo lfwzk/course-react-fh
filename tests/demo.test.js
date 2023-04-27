85aa5b731bacf23d131661026dc6f332fc3818f5
Fernando cs <fwzk777@gmail.com>
2023-04-27 19:34:05 -0400

    jest clase 1

---
+++ tests/demo.test.js
@@ -0,0 +1,7 @@
+describe("pruebas jest", () => {
+  test("prueba no debe de fallar", () => {});
+  //1.inicializzacion
+  //2. estimulacion
+  //3. observar el comportamiento
+  // expect(message1).toBe;
+});
